cb11e6222ea0839c66b32b8429b123ad387a53da
David <dyarzebinski97@gmail.com>
2020-01-14 23:03:25 -0500

    done

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -6,8 +6,28 @@ const Uses = () => (
     <h1>Spilt Coffee and Lovely Terminals</h1>
     <p>
       These are a few of the things that I use on a daily basis for work and/or
-      hobby
+      hobby.
     </p>
+
+    <h4>Hardware</h4>
+    <ul>
+      <li>Custom Built Desktop</li>
+      <li>Akko 3068 with Cherry MX Reds</li>
+      <li>ASUS 24" 144hz monitor</li>
+      <li>Logitech G502 Mouse</li>
+      <li>Audio Technica MX50 Headphones</li>
+      <li>Lenovo Touchpad X2</li>
+      <li>Blue Yeti Microphone</li>
+    </ul>
+
+    <h4>Software</h4>
+    <p>The bread and the butter</p>
+    <ul>
+      <li>VSCode - Dont think any other editor compares anymore</li>
+      <li>Cmder and Windows Terminal - Going back and forth anymore</li>
+      <li>Notable for note taking</li>
+      <li>Google Chrome</li>
+    </ul>
   </Layout>
 );
 
