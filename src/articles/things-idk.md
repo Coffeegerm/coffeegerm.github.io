0ecf1473170191f8b0009eda68d9482377c80d57
David <davidyarzebinski@cloudfitsoftware.com>
2020-01-25 23:25:48 -0500

    things idk

---
+++ src/articles/things-idk.md
@@ -0,0 +1,26 @@
+---
+path: '/blog/things-idk'
+date: '2020-01-26'
+title: "Things I Don't Know, But Want To"
+description: "A small list of things that I don't know but have an interest in."
+---
+
+One of the greatest and scariest things about being a developer is the vast amount of things there are to learn, to play with, and to master. It can be amazing to switch between frameworks such as React and Vue within web development or Unity and Unreal in game development, C# and Go for databases, but doing this comes at a cost.
+
+Switching carelessly between one thing to another never lets you get running with those frameworks or languages. You skim the surface of these things and never get down and dirty with them, learning quarks and oddities in the languages and frameworks that you could help to fix in open sourced langauges and packages or teach others a work around that you have built for a common problem.
+
+In 2020, I would like to learn at 3 new things at a beginner to intermediary level. These things are things that peak my interest as a developer and I want to use some of my free time to read about the languages, play with them, and possibly build a project with them to get a grasp of them.
+
+1. Go
+
+Go is extremely hot right now, listening to podcasts about it I hear nothing but praise about the language and its capabilities with only its standard library. This makes me very intrigued with it. Coming from web development with React that has a great number of packages that you need to make a single application and C#/Entity framework I think Go might be a an amazing language to learn.
+
+2. Unity
+
+I play games and fantisize about making a game all my own of course. Hasnt everyone that enjoys video games? Unity looks and reads like its very straightforward and might be option that I choose in the future to learn and develop with.
+
+3. Rust
+
+I have never taken a deep dive into a lower level capable language and Rust seems to be the de facto language for going deepeinto the computer sub system at the moment. Jobs are booming for this language and I'm fascinated by what all the Rustaceans arlosing their mind over.
+
+These are a few high level things that I am looking forward to learning about in the future, of course I am constantly trying to get better at everything I am doing including database structuring, C#, React, and design.
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,93 +1,83 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
 
 import Address from './Address';
 
-class Contact extends PureComponent {
-  constructor(props) {
-    super(props);
+const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
-    };
-  }
-
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
+  const handleNameChange = event => {
+    setName(event.target.value);
   };
 
-  handleEmailChange = event => {
-    this.setState({ email: event.target.value });
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
   };
 
-  handleMessageChange = event => {
-    this.setState({ message: event.target.value });
+  const handleMessageChange = event => {
+    setMessage(event.target.value);
   };
 
-  render() {
-    const { name, email, message } = this.state;
-
-    return (
-      <section id="four">
-        <h2>Talk to Me</h2>
-        <p>
-          If interested in communicating with me, feel free to send me a message
-          through Twitter or an email. I&apos;m always willing to chat about
-          code or other things!
-        </p>
-        <div className="row">
-          <div className="8u 12u$(small)">
-            <form
-              action={`mailto:dyarzebinski97@gmail.com?subject=Let's Talk!`}
-              method="POST"
-              encType="text/plain"
-            >
-              <div className="row uniform 50%">
-                <div className="6u 12u$(xsmall)">
-                  <input
-                    type="text"
-                    name="Name"
-                    id="name"
-                    placeholder="Name"
-                    onChange={this.handleNameChange}
-                    value={name}
-                  />
-                </div>
-                <div className="6u 12u$(xsmall)">
-                  <input
-                    type="email"
-                    name="Email"
-                    id="email"
-                    placeholder="Email"
-                    onChange={this.handleEmailChange}
-                    value={email}
-                  />
-                </div>
-                <div className="12u">
-                  <textarea
-                    name="Message"
-                    id="message"
-                    placeholder="Message"
-                    rows="4"
-                    onChange={this.handleMessageChange}
-                    value={message}
-                  />
-                </div>
+  return (
+    <section id="four">
+      <h2>Talk to Me</h2>
+      <p>
+        If interested in communicating with me, feel free to send me a message
+        through Twitter or an email. I&apos;m always willing to chat about code
+        or other things!
+      </p>
+      <div className="row">
+        <div className="8u 12u$(small)">
+          <form
+            action={`mailto:dyarzebinski97@gmail.com?subject=Let's Talk!`}
+            method="POST"
+            encType="text/plain"
+          >
+            <div className="row uniform 50%">
+              <div className="6u 12u$(xsmall)">
+                <input
+                  type="text"
+                  name="Name"
+                  id="name"
+                  placeholder="Name"
+                  onChange={handleNameChange}
+                  value={name}
+                />
               </div>
-              <ul className="actions slightMarginToTop">
-                <li>
-                  <input type="submit" value="Send Message" />
-                </li>
-              </ul>
-            </form>
-          </div>
-
-          <Address />
+              <div className="6u 12u$(xsmall)">
+                <input
+                  type="email"
+                  name="Email"
+                  id="email"
+                  placeholder="Email"
+                  onChange={handleEmailChange}
+                  value={email}
+                />
+              </div>
+              <div className="12u">
+                <textarea
+                  name="Message"
+                  id="message"
+                  placeholder="Message"
+                  rows="4"
+                  onChange={handleMessageChange}
+                  value={message}
+                />
+              </div>
+            </div>
+            <ul className="actions slightMarginToTop">
+              <li>
+                <input type="submit" value="Send Message" />
+              </li>
+            </ul>
+          </form>
         </div>
-      </section>
-    );
-  }
-}
+
+        <Address />
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
